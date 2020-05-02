c37340dec8c160a93d21627556c4c3cb19cc543f
Mob-912 <64009800+Mob-912@users.noreply.github.com>
2020-05-02 23:06:09 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 const movieRouter = require('./router/book_router');
 app.use(movieRouter);
 
